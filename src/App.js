c9804c587843554af2820485b8f49ea98ac88f75
foxintellivan <ivan@foxintelligence.fr>
2020-08-06 21:03:06 +0200

    always dark

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import './App.css';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import SongListPage from './containers/SongListPage.js'
 
 function App() {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-
   const theme = React.useMemo(
     () =>
       createMuiTheme({
         palette: {
-          type: prefersDarkMode ? 'dark' : 'light',
+          type: 'dark'
         },
       }),
-    [prefersDarkMode],
+    [],
   );
   return (
     <ThemeProvider theme={theme}>
