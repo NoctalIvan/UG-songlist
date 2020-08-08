1124d5a7b3e83d0cf81961f85fc22b59b3d18453
foxintellivan <ivan@foxintelligence.fr>
2020-08-08 12:00:48 +0200

    artist types + filter songlist

---
+++ src/tools/extractArtistsToTypes.js
@@ -0,0 +1,7 @@
+/* Converts webSongBook_export.csv into JSON file */
+
+const fs = require('fs');
+const songs = require('../data/songList.json')
+
+const artists = [...new Set(songs.map(s => s.artist))].map(a => ({artist: a, type: null}))
+fs.writeFileSync('./../data/artistTypes.json', JSON.stringify(artists, null, 2))
